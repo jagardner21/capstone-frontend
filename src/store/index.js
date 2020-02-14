d9fe465a17d99e8f07ba891f20ec8a27d6bd21ca
jagardner21 <42302459+jagardner21@users.noreply.github.com>
2020-02-13 18:40:34 -0700

    redux is complete...as long as it works when I add a UI

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'thunk'
+import logger from 'redux-logger'
+import eventsReducer from './events/reducer'
+import teamsReducer from './teams/reducer'
+import usersReducer from './users/reducer'
+
+const rootReducer = combineReducers({
+    events: eventsReducer,
+    teams: teamsReducer,
+    users: usersReducer
+})
+
+const middleware = [thunk, logger]
+
+export default createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)))
+
